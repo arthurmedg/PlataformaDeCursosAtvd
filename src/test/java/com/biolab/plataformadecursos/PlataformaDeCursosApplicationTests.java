1611abfb40a013c57fdff2de4449e1434718b734
arthurmedg <arthurmedg@gmail.com>
2026-09-18 08:28:48 -0300

    feat: criação do projeto

---
+++ src/test/java/com/biolab/plataformadecursos/PlataformaDeCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biolab.plataformadecursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDeCursosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
